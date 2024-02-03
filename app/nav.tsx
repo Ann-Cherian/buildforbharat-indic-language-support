27a5549ce34f5fb82d4a6c44901f65fe4934f4b8
Ann B Cherian <annbyjus@gmail.com>
2024-02-03 23:16:59 +0530

    feat: navigation bar

---
+++ app/nav.tsx
@@ -0,0 +1,11 @@
+export default function Nav() {
+  return (
+    <div className="flex justify-between p-6 border-b">
+      <h1>Discover</h1>
+      <h1>Search</h1>
+      <h1>Cart</h1>
+      <h1>Orders</h1>
+      <h1>Customer Support</h1>
+    </div>
+  );
+}
